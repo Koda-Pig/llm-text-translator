6d9815a207d93ef6cda8b5cd6273a32179809e67
Josh Koter <josh.koter@gmail.com>
2024-12-05 17:44:49 +0200

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { ChatOpenAI } from "@langchain/openai";
+import { HumanMessage, SystemMessage } from "@langchain/core/messages";
+import { AIMessageChunk } from "@langchain/core/messages";
+import "./App.css";
+
+const API_KEY = import.meta.env.VITE_OPENAI_API_KEY;
+const model = new ChatOpenAI({ model: "gpt-4", apiKey: API_KEY });
+const messages = [
+  new SystemMessage("Translate the following from English to Italian"),
+  new HumanMessage("Goodbye my dear friend")
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [response, setResponse] = useState<AIMessageChunk | null>(null);
+
+  useEffect(() => {
+    const invoke = async () => {
+      const responseData = await model.invoke(messages);
+
+      if (!responseData) {
+        throw new Error("No response from model");
+      }
+
+      setResponse(responseData);
+    };
+
+    invoke();
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        {typeof response?.content === "string" && <p>{response?.content}</p>}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
